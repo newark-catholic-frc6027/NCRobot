53366e873668ba830f97055c4620adad7da7a64e
Jason Mihalick <jason@jcraftsmen.com>
2018-03-18 18:32:51 -0400

    Move initialization of pnuematicsubsystem to teleopInit and AutonomousInit

--- src/main/java/org/usfirst/frc/team6027/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team6027/robot/Robot.java
@@ -66,15 +66,7 @@ public class Robot extends IterativeRobot {
         this.setOperatorDisplay(new OperatorDisplaySmartDashboardImpl());
         this.setOperatorInterface(new OperatorInterface(this.getOperatorDisplay()));
         this.setDrivetrain(new DrivetrainSubsystem(this.getOperatorInterface()));
-        this.setPneumaticSubsystem(new PneumaticSubsystem(this.getOperatorDisplay()));
         this.setElevatorSubsystem(new ElevatorSubsystem(this.getSensorService().getLimitSwitchSensors()));
-        
-
-        // This ensures that the Teleop command is running whenever we are not in
-        // autonomous mode
-        TeleopManager teleOpCommand = new TeleopManager(this.operatorInterface, this.sensorService,
-                this.getDrivetrain(), this.pneumaticSubsystem, this.elevatorSubsystem);
-        this.getDrivetrain().setDefaultCommand(teleOpCommand);
 
         AutonomousCommandManager.initAutoScenarioDisplayValues(this.getOperatorDisplay());
         AutonomousCommandManager.initUnlessOptionDisplayValues(this.getOperatorDisplay());
@@ -143,6 +135,8 @@ public class Robot extends IterativeRobot {
     @Override
     public void autonomousInit() {
         
+        this.setPneumaticSubsystem(new PneumaticSubsystem(this.getOperatorDisplay()));
+        
         applyStationPosition();
         String preferredAutoScenario = this.getOperatorDisplay().getSelectedAutoScenario();
         String unlessOption = this.getOperatorDisplay().getSelectedUnlessOption();
@@ -208,6 +202,14 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void teleopInit() {
+        // This ensures that the Teleop command is running whenever we are not in
+        // autonomous mode
+        this.setPneumaticSubsystem(new PneumaticSubsystem(this.getOperatorDisplay()));
+        
+        TeleopManager teleOpCommand = new TeleopManager(this.operatorInterface, this.sensorService,
+                this.getDrivetrain(), this.pneumaticSubsystem, this.elevatorSubsystem);
+        this.getDrivetrain().setDefaultCommand(teleOpCommand);
+
         // This makes sure that the autonomous stops running when
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
