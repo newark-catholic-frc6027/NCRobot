afd16c46fb2f70b4e46772ac619a02064ab24989
Jason Mihalick <jason@jcraftsmen.com>
2018-03-23 20:18:30 -0400

    Tuning distances

--- src/main/java/org/usfirst/frc/team6027/robot/commands/autonomous/AutoDeliverToScaleEndFromOppositeSide.java
+++ src/main/java/org/usfirst/frc/team6027/robot/commands/autonomous/AutoDeliverToScaleEndFromOppositeSide.java
@@ -88,11 +88,11 @@ public class AutoDeliverToScaleEndFromOppositeSide extends CommandGroup {
     }
     
     protected Command createMultiLegDriveCommand() {
-        double leg1Distance = 200.0;//this.prefs.getDouble("leg1.distance", 12.0);
+        double leg1Distance = this.prefs.getDouble("leg1.distance", 200.0);
         double leg1Angle = 0.0;//this.prefs.getDouble("leg1.angle", 0.0);
-        double leg2Distance = 215.0;//this.prefs.getDouble("leg2.distance", 47.0);
+        double leg2Distance = this.prefs.getDouble("leg2.distance", 215.0);
         double leg2Angle = 90.0 * (this.deliverySide == DeliverySide.Right ? -1.0 : 1.0);//this.prefs.getDouble("leg2.angle", 30.0) * (this.deliverySide == DeliverySide.Right ? 1.0 : -1.0);
-        double leg3Distance = 70.0;//this.prefs.getDouble("leg3.distance", 100.0);
+        double leg3Distance = this.prefs.getDouble("leg3.distance", 70.0);
         double leg3Angle = 0.0;
 
         TargetVector[] turnVectors = new TargetVector[] { 
