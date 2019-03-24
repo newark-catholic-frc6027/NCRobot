89d56e21334e16e9e804c9ff85d689722a5379a0
Jason Mihalick <jason@jcraftsmen.com>
2019-03-23 22:16:43 -0400

    Add log pruning

--- src/main/java/frc/team6027/robot/RobotShutdownHook.java
+++ src/main/java/frc/team6027/robot/RobotShutdownHook.java
@@ -5,25 +5,45 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.comparator.LastModifiedFileComparator;
 
 public class RobotShutdownHook extends Thread {
     private final Logger logger = LogManager.getLogger(getClass());
+    public static final int MAX_LOG_COUNT = 5;
     private final static File frcLogFile = new File("/var/local/natinst/log/FRC_UserProgram.log");
     private final static File logArchiveDir = new File("/home/lvuser/frclog-backups");
     private final static SimpleDateFormat ts = new SimpleDateFormat("yyyyMMdd_hhmmss");
     public RobotShutdownHook() {
         createLogArchiveDir();
+        pruneLogs();
     }
 
+    public void pruneLogs() {
+        try {
+            if (logArchiveDir.exists()) {
+                File[] logFiles = logArchiveDir.listFiles();
+                if (logFiles != null && logFiles.length > MAX_LOG_COUNT) {
+                    Arrays.sort(logFiles, LastModifiedFileComparator.LASTMODIFIED_REVERSE);
+                    for (int i = 0; i < logFiles.length - MAX_LOG_COUNT; i++) {
+                        logFiles[i].delete();
+                        logger.info("Pruned old log file '{}", logFiles[i].getPath());
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            logger.error("Failure while pruning logs. Error: {}", ex.getMessage());
+        }
+    }
     protected void createLogArchiveDir() {
         if (! logArchiveDir.exists()) {
             if (! logArchiveDir.mkdirs()) {
                 logger.warn("Failed to create log archive dir '{}''", this.logArchiveDir.getPath());
             }
-            // TODO: prune old logs
         }
     }
 
