6582698bbbec397375f0a90e71abbdce7e39fc55
Jason Mihalick <jason@jcraftsmen.com>
2019-03-26 00:28:53 -0400

    Fix DelayCommand

--- src/main/java/frc/team6027/robot/commands/autonomous/DelayCommand.java
+++ src/main/java/frc/team6027/robot/commands/autonomous/DelayCommand.java
@@ -1,36 +1,43 @@
 package frc.team6027.robot.commands.autonomous;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import edu.wpi.first.wpilibj.command.Command;
 
-
-public class DelayCommand  extends Command {
+public class DelayCommand extends Command {
+    private final Logger logger = LogManager.getLogger(getClass());
 
     protected int delayMs;
-    protected long startTime;
-    protected long finishTime; 
+    protected Long startTime;
+    protected Long finishTime = null;
 
-    public DelayCommand(int delayMs) {    
+    public DelayCommand(int delayMs) {
 
         this.setName("No Command");
         this.delayMs = delayMs;
 
     }
-    @Override
-    public void start() {
-        this.startTime = System.currentTimeMillis();
-        this.finishTime = startTime + delayMs;
 
+    @Override
+    protected void execute() {
+        if (this.finishTime == null) {
+            this.logger.info(">>>>>>>>>>>>>>>>>>>> {} command STARTING", this.getClass().getSimpleName());
+            this.startTime = System.currentTimeMillis();
+            this.finishTime = this.startTime + this.delayMs;
         }
+
+    }
 
     @Override
     protected boolean isFinished() {
-      if(System.currentTimeMillis() >= finishTime) {
-     
-        return true;
-
+        if (System.currentTimeMillis() >= this.finishTime) {
+            this.logger.info(">>>>>>>>>>>>>>>>>>>> {} command FINISHED", this.getClass().getSimpleName());
+            this.finishTime = null;
+            this.startTime = null;
+            return true;
+        } else {
+            return false;
         }
-    else {
-        return false;
-  }
- }
+    }
 }
